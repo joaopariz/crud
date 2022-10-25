bedabdfbd2174fec0d52c63d5690f9f4de187c2a
João Pariz <parizoficial0102@outlook.com>
2022-10-24 23:54:34 -0300

    add crud

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$bd_name = 'crud';
+
+$connx = mysqli_connect($host, $user, $password, $bd_name);
+
+?>
+
---
+++ excluir.php
@@ -0,0 +1,14 @@
+<?php 
+
+include 'conexao.php';
+
+$id = $_POST['id'];
+
+$recebendo_cadastros = "DELETE FROM tb_cadastro WHERE id='$id'";
+
+$query_cadastros = mysqli_query($connx, $recebendo_cadastros);
+
+header('location:listagem.php');
+
+?>
+
